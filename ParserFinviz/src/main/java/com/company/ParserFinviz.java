3d0bc136a10135a9d146e4ca9e38222be4550863
MaxTsyp <cmsstv@gmail.com>
2019-05-18 03:40:52 +0300

    Add proj

---
+++ ParserFinviz/src/main/java/com/company/ParserFinviz.java
@@ -0,0 +1,118 @@
+package com.company;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import java.io.*;
+import java.net.URL;
+
+
+
+public class ParserFinviz {
+    public static String name = "row";
+    public static String[] rowresult;
+    static String filename = "C:\\Users\\Max\\Desktop\\NewExcelFile.xls" ;
+    static HSSFWorkbook workbook = new HSSFWorkbook();
+    public  static HSSFSheet sheet = workbook.createSheet("FirstSheet");
+
+    static int number;
+
+    public void CreatRow(String row) throws IOException {
+            int i = 0;
+
+
+            HSSFRow row1 = sheet.createRow((short)i++);
+            row1.createCell(0).setCellValue(row);
+
+
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            workbook.write(fileOut);
+            fileOut.close();
+            System.out.println("Your excel file has been generated!");
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        parserChartmill.ParsNumberPage();
+        System.out.println("Страниц парсим: " + number);
+
+        parserChartmill.ParsFirstPage();
+
+      parserChartmill.Pars();
+
+    }
+    static ParserFinviz parserChartmill = new ParserFinviz();
+
+    public static Elements results;
+    public static String tickerData;
+    public static String[] resultsFinal;
+    private String url = "https://finviz.com/screener.ashx?v=141&f=sh_avgvol_o100,sh_price_u15,sh_relvol_o1&ft=4";
+            //Main.globalUrl ;
+
+
+    private static Document getPage(String url) throws IOException {
+        Document page = Jsoup.parse(new URL(url), 5000);
+        return page;
+    }
+
+    public void ParsFirstPage() throws IOException  {
+        Document page = getPage(url);
+        Elements table = page.select("table [width=100%]");
+        results = table.select("tr [class=screener-body-table-nw]");
+        Elements elements = results.select("[class=screener-link-primary]");
+
+        PrintWriter writer = new PrintWriter("TICKER.txt", "UTF-8");
+        rowresult = elements.text().split(" ");
+        System.out.println(rowresult.length);
+
+        writer.println(elements.text());
+
+        writer.close();
+
+
+       // System.out.println(elements.text());
+    }
+
+    public void Pars() throws IOException  {
+            String url2 = "&r=";
+            int url3 = 21;
+
+            for (int i = 1; i < number ; i++) {
+
+            Document page2 = getPage(url + url2 + url3);
+
+            Elements table = page2.select("table [width=100%]");
+
+            results = table.select("tr [class=screener-body-table-nw]");
+            Elements elements = results.select("[class=screener-link-primary]");
+            tickerData = elements.text();
+            System.out.println(tickerData);
+
+                try {
+                    PrintWriter prWr = new PrintWriter(new BufferedWriter(new FileWriter("TICKER.txt", true)));
+                    prWr.println(tickerData);
+                    prWr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            url3 = url3 + 20;
+        }
+    }
+    public void ParsNumberPage() throws IOException {
+        Document page2 = getPage(url);
+        Elements numberpage = page2.select("a");
+        Elements num = numberpage.select("[class=screener-pages]");
+
+        String[] number1 = num.text().split(" ");
+
+        String number2 = number1[number1.length -1];
+        number = Integer.parseInt(number2);
+
+    }
+
+}
